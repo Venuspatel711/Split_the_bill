afd775ffb243cb4c3299d5ea2d65087b9c5c935e
Venuspatel711 <160982867+Venuspatel711@users.noreply.github.com>
2024-04-18 04:08:32 +0000

    i have created 3 more methods

--- SplitBill/UnitTest1.cs
+++ SplitBill/UnitTest1.cs
@@ -5,6 +5,7 @@ namespace SplitBill
     [TestClass]
     public class Splitcalc
     {
+     // test cases for firts method.   
         [TestMethod]
         public void SplitAmount_WhenAmountIs100AndNumberOfPeopleIs5_ShouldReturn20()
         {
@@ -43,6 +44,48 @@ namespace SplitBill
            
             Assert.AreEqual(20, result);
         }
-        
+        // test cases for second method
+        [TestMethod]
+        public void CalculateTipPerPerson_WithValidInput_ShouldReturnCorrectTipAmount()
+        {
+            Split_Lib.Splitcalc calculator = new Split_Lib.Splitcalc();
+            decimal price = 100.00m;
+            int numberOfPatrons = 5;
+            float tipPercentage = 15;
+            decimal expectedTipPerPerson = 3.00m;
+
+            decimal actualTipPerPerson = calculator.CalculateTipPerPerson(price, numberOfPatrons, tipPercentage);
+
+            Assert.AreEqual(expectedTipPerPerson, actualTipPerPerson, "Tip per person should be 3.00");
+        }
+        [TestMethod]
+        public void CalculateTipPerPerson_WithZeroPrice_ShouldReturnZeroTipPerPerson()
+        {
+            // Arrange
+           Split_Lib.Splitcalc calculator = new Split_Lib.Splitcalc();
+            decimal price = 0m;
+            int numberOfPatrons = 5;
+            float tipPercentage = 15;
+
+            // Act
+            decimal actualTipPerPerson = calculator.CalculateTipPerPerson(price, numberOfPatrons, tipPercentage);
+
+            // Assert
+            Assert.AreEqual(0m, actualTipPerPerson, "Tip per person should be 0.00 for zero price");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculateTipPerPerson_WithZeroPatrons_ShouldThrowArgumentException()
+        {
+            // Arrange
+            Split_Lib.Splitcalc calculator = new Split_Lib.Splitcalc();
+            decimal price = 100.00m;
+            int numberOfPatrons = 0;
+            float tipPercentage = 15;
+
+            // Act (and Assert)
+            decimal actualTipPerPerson = calculator.CalculateTipPerPerson(price, numberOfPatrons, tipPercentage);
+        }
     }
 }
--- Split_Lib/Splitcalc.cs
+++ Split_Lib/Splitcalc.cs
@@ -2,17 +2,44 @@ using System;
 
 namespace Split_Lib;
 
-    public class Splitcalc
-    {
-      public decimal SplitAmount(decimal amount, int numberOfPeople)
-{
-    if (numberOfPeople <= 0)
-    {
-        throw new ArgumentException("Number of people must be greater than zero.");
-    }
+        public class Splitcalc
+        {
+        public decimal SplitAmount(decimal amount, int numberOfPeople)
+         {
+        if (numberOfPeople <= 0)
+        {
+            throw new ArgumentException("Number of people must be greater than zero.");
+        }
 
-    return amount / numberOfPeople;
+        return amount / numberOfPeople;
+        }
+        
+
+        public decimal CalculateTipPerPerson(decimal price, int numberOfPatrons, float tipPercentage)
+        {
+            if (numberOfPatrons <= 0)
+                throw new ArgumentException("Number of patrons must be greater than zero.");
+
+            decimal totalTip = (decimal)tipPercentage / 100 * price;
+            return totalTip / numberOfPatrons;
+        }
+        public Dictionary<string, decimal> CalculateTipPerPerson(Dictionary<string, decimal> mealCosts, float tipPercentage)
+        {
+            decimal totalMealCost = 0;
+            foreach (var pair in mealCosts)
+            {
+                totalMealCost += pair.Value;
+            }
+
+            var tipAmounts = new Dictionary<string, decimal>();
+            foreach (var pair in mealCosts)
+            {
+                decimal tipAmount = (decimal)tipPercentage / 100 * pair.Value;
+                decimal weightedAverage = tipAmount / totalMealCost;
+
+                tipAmounts.Add(pair.Key, weightedAverage);
+            }
+
+            return tipAmounts;
+        }
 }
-
-
-    }
